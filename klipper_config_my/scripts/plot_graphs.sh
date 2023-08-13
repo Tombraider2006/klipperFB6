37e886a328e42d08ae059947d8c2bc5e39725dd7
Tombraider2006 <59514540+Tombraider2006@users.noreply.github.com>
2023-08-13 18:12:11 +0300

    обнова

--- klipper_config_my/scripts/plot_graphs.sh
+++ klipper_config_my/scripts/plot_graphs.sh
@@ -5,32 +5,7 @@
 # Written by Frix_x#0161 #
 # @version: 1.6
 
-# CHANGELOG:
-#   v1.6: - updated the handling of shaper graph files to be able to optionnaly account for added positions in the filenames and remove them
-#         - fixed a bug in the belt graph on slow SD card or Pi clones (Klipper was still writing in the file while we were already reading it)
-#   v1.5: fixed klipper unnexpected fail at the end of the execution, even if graphs were correctly generated (unicode decode error fixed)
-#   v1.4: added the ~/klipper dir parameter to the call of graph_vibrations.py for a better user handling (in case user is not "pi")
-#   v1.3: some documentation improvement regarding the line endings that needs to be LF for this file
-#   v1.2: added the movement name to be transfered to the Python script in vibration calibration (to print it on the result graphs)
-#   v1.1: multiple fixes and tweaks (mainly to avoid having empty files read by the python scripts after the mv command)
-#   v1.0: first version of the script based on a Zellneralex script
 
-# Installation:
-#   1. Copy this file somewhere in your config folder and edit the parameters below if needed
-#      Note: If using Windows to do the copy/paste, be careful with the line endings for this file: LF (or \n) is mandatory !!! No \r should be
-#            present in the file as it could lead to some errors like "\r : unknown command" when running the script. If you're not confident
-#            regarding your text editor behavior, the best way is to directly download the file on the pi by using for example wget:
-#            type 'wget -P ~/printer_data/config/scripts https://raw.githubusercontent.com/Frix-x/klippain/main/scripts/plot_graphs.sh'
-#   2. Make it executable using SSH: type 'chmod +x ~/printer_data/config/scripts/plot_graphs.sh' (adjust the path if needed).
-#   3. Be sure to have the gcode_shell_command.py Klipper extension installed (easiest way to install it is to use KIAUH in the Advanced section)
-#   4. Create a gcode_shell_command to be able to start it from a macro (see my shell_commands.cfg file)
-
-# Usage:
-#   This script was designed to be used with gcode_shell_commands. Use it to call it.
-#   Parameters availables:
-#      SHAPER      - To generate input shaper diagrams after calling the Klipper TEST_RESONANCES AXIS=X/Y
-#      BELTS       - To generate belts diagrams after calling the Klipper TEST_RESONANCES AXIS=1,(-)1 OUTPUT=raw_data
-#      VIBRATIONS  - To generate vibration diagram after calling the custom (Frix_x#0161) VIBRATIONS_CALIBRATION macro
 
 
 #################################################################################################################
@@ -114,7 +89,8 @@ function plot_belts_graph {
   sync && sleep 2
 
   # Finally we compute the belts graph
-  "${generator}" -c "${isf}"/belts/belt_"${date_ext}"_*.csv -o "${isf}"/belts/belts_"${date_ext}".png
+  "${generator}" -c "${isf}"/belts/belt_"${date_ext}"_*.csv -o "${isf}"/belts/belts.png
+#  "${generator}" -c "${isf}"/belts/belt_"${date_ext}"_*.csv -o "${isf}"/belts/belts_"${date_ext}".png
 }
 
 function plot_vibr_graph {
@@ -136,8 +112,8 @@ function plot_vibr_graph {
   sync && sleep 2
 
   # We compute the vibration graphs using all the csv files
-  "${generator}" "${isf}"/vibrations/vibr_"${date_ext}"*.csv -o "${isf}"/vibrations/vibrations_"${date_ext}".png -a "$1" -k "${KLIPPER_FOLDER}"
-
+  "${generator}" "${isf}"/vibrations/vibr_"${date_ext}"*.csv -o "${isf}"/vibrations/vibrations.png -a "$1" -k "${KLIPPER_FOLDER}"
+#  "${generator}" "${isf}"/vibrations/vibr_"${date_ext}"*.csv -o "${isf}"/vibrations/vibrations_"${date_ext}".png -a "$1" -k "${KLIPPER_FOLDER}"
   # Finally we cleanup the folder by moving the csv files in an archive
   tar cfz "${isf}"/vibrations/vibrations_"${date_ext}".tar.gz "${isf}"/vibrations/vibr_"${date_ext}"*.csv
   rm "${isf}"/vibrations/vibr_"${date_ext}"*.csv
